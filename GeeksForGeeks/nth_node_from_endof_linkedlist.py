e109f0617e7cdb80081a17937533918a25c22116
Johns Jijo Sabu <jijojohns12.5@gmail.com>
2021-04-13 11:27:03 +0530

    13th commit

---
+++ GeeksForGeeks/nth_node_from_endof_linkedlist.py
@@ -0,0 +1,66 @@
+'''
+	Your task is to return the data stored in
+	the nth end from end of linked list.
+	
+	Function Arguments: head (reference to head of the list), n (pos of node from end)
+	Return Type: Integer or -1 if no such node exits.
+
+	{
+		# Node Class
+		class Node:
+		    def __init__(self, data):   # data -> value stored in node
+		        self.data = data
+		        self.next = None
+	}
+'''
+
+
+def getNthfromEnd(head,n):
+    cur = head
+    length = 0
+    while cur:
+        length += 1
+        cur = cur.next
+    if n > length:
+        return -1
+    cur = head
+    pos = (length-n) + 1
+    ind = 1
+    while cur:
+        if ind == pos:
+            return cur.data
+        cur = cur.next
+        ind += 1
+    
+
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+# Linked List Class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    # creates a new node with given value and appends it at the end of the linked list
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            return
+        self.tail.next = new_node
+        self.tail = new_node
+
+if __name__ == '__main__':
+    t=int(input())
+    for cases in range(t):
+        n,nth_node = map(int, input().strip().split())
+        a = LinkedList() # create a new linked list 'a'.
+        nodes_a = list(map(int, input().strip().split()))
+        for x in nodes_a:
+            a.append(x)  # add to the end of the list
+        print(getNthfromEnd(a.head,nth_node))
